9f9ced16552e8e2352b279084106bc31c41e9140
Fernando Pauer <fpauer@gmail.com>
2016-12-30 12:25:07 -0800

    simple maven project

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by fpauer
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by fpauer
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
